71bddf35bfb29ef4abed93507d105333781746fb
Fias7777 <102873032+Fias7777@users.noreply.github.com>
2023-09-03 05:23:43 +0300

    add gitignore,add Bean(Cat)

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,11 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        Cat bean1 = (Cat) applicationContext.getBean("cat");
+        Cat bean2 = (Cat) applicationContext.getBean("cat");
+
+        System.out.println("HelloWorld: " + (bean == bean));
+        System.out.println("Cat: " + (bean1 == bean2));
+
     }
 }
